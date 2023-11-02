e2cf182606644eff23050a20dfd623e349f39bb2
Joey Kiesau <kiesau31@gmail.com>
2023-11-02 07:59:36 -0500

    JK photography

---
+++ album.js
@@ -0,0 +1,24 @@
+const categories = ['events', 'products', 'portraits'];
+let currentCategoryIndex = 0;
+
+function changeCategory(indexChange) {
+    currentCategoryIndex += indexChange;
+
+    if (currentCategoryIndex < 0) {
+        currentCategoryIndex = categories.length - 1;
+    }
+
+    if (currentCategoryIndex >= categories.length) {
+        currentCategoryIndex = 0;
+    }
+
+    const currentCategory = categories[currentCategoryIndex];
+    const currentCategoryTitle = currentCategory.charAt(0).toUpperCase() + currentCategory.slice(1);
+
+    document.getElementById('album-title').textContent = currentCategoryTitle;
+    document.getElementById('album-image').src = `images/${currentCategory}1.jpg`;
+
+    // Adjust the view all link
+    const viewAllLink = document.querySelector('.view-all');
+    viewAllLink.href = `view_all_${currentCategory}.html`;
+}
---
+++ script.js
@@ -0,0 +1,42 @@
+function toggleMenu(x) {
+   x.classList.toggle("change");
+   var menuDropdown = document.getElementById("menuDropdown");
+   menuDropdown.style.display = (menuDropdown.style.display === "block") ? "none" : "block";
+}
+// Add smooth scrolling behavior
+document.addEventListener('DOMContentLoaded', function() {
+   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+       anchor.addEventListener('click', function(e) {
+           e.preventDefault();
+
+           document.querySelector(this.getAttribute('href')).scrollIntoView({
+               behavior: 'smooth'
+           });
+       });
+   });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuLinks = document.querySelectorAll('.menu-dropdown a');
+
+    menuLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            var targetId = this.getAttribute('href').substring(1);
+            var targetElement = document.getElementById(targetId);
+
+            var offset = targetId === 'about-section' ? 300 : 
+            targetId === 'services-section' ? 300 : 
+            targetId === 'albums-section' ? 100 : 0; // Adjust the value as needed
+
+            window.scrollTo({
+                top: targetElement.offsetTop + offset,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+
+
